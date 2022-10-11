46648c903cb9d15560e69f3e25579687b76f149f
ccr5 <matt-gomes@live.com>
2022-10-11 07:04:05 -0300

    Part 4 Ok, starting to comment the code

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,41 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     let contents: String = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    /*
+    'a define a explicit lifetime.
+    Rust require you to connect arguments to return values in the signature.
+    */
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query: &str = "duct";
+        let contents: &str = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
