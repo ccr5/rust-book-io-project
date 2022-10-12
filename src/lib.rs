b5a5447bf8564142f042227f3037dbcfe98dabf1
ccr5 <matt-gomes@live.com>
2022-10-12 07:26:08 -0300

    case insensitive implementation Ok

--- src/lib.rs
+++ src/lib.rs
@@ -71,6 +71,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query: String = query.to_lowercase();
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 /*
 cfg = Configuration conditional checks are possible through two different operators
 https://doc.rust-lang.org/rust-by-example/attribute/cfg.html
@@ -86,13 +99,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query: &str = "duct";
         let contents: &str = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query: &str = "rUsT";
+        let contents: &str = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
